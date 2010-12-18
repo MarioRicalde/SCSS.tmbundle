19fa025274d4ff1f4cbe9b9e081b7cb1a2e6617b
Mario Kuroir Ricalde <kuroir@gmail.com>
2010-12-18 03:44:29 -0600

    Adds commands to easily increase-decrease numbers (ctrl + up/down). Just place your cursor on any number and do it should work.

---
+++ Support/bin/scroller.rb
@@ -0,0 +1,12 @@
+def scroller(type, q = 1)
+  if result = ENV['TM_CURRENT_WORD'].match(/(\d+)(\w+)?/)
+    int = result[1].to_i
+    case type
+    when 'add'
+      int += q
+    when 'substract'
+      int -= q
+    end
+    print int.to_s << result[2]
+  end
+end
