3591edc5a3918158a48560dc40404eca8b67f65e
Mario Kuroir Ricalde <kuroir@gmail.com>
2011-01-02 12:39:11 +0800

    control+down now allows to go to negative numbers. -1 goes to -2.

--- Support/bin/scroller.rb
+++ Support/bin/scroller.rb
@@ -1,12 +1,11 @@
 def scroller(type, q = 1)
-  if result = ENV['TM_CURRENT_WORD'].match(/(\d+)(\w+)?/)
+  if result = ENV['TM_CURRENT_WORD'].match(/(\-?\d+)(\w+)?/)
     int = result[1].to_i
     case type
     when 'add'
       int += q
     when 'substract'
-      int -= q
-      int = 0 if int < 0
+      int -= 1
     end
     print int.to_s << result[2].to_s
   end
